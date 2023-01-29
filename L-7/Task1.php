b89c2ceab872530ac8bae72ee60991cbfefc7856
Vika Zhurauliova <122206989+VikaZhurauliova@users.noreply.github.com>
2023-01-29 20:45:11 +0300

    Add files via upload

---
+++ L-7/Task1.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['text'];
+$result = preg_match('/^[\S\w]{2,20}\.(png|jpeg|jpg|pdf)$/', $name);
+if ($result){
+    if ($_FILES) {
+        $filename = __DIR__ . '/' . $name;
+        move_uploaded_file($_FILES['file']['tmp_name'],$filename);
+    }
+} else {
+    return 'error';
+}
